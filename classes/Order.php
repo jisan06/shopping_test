f36456ed85c98f88ea5cf7a68d59805a213dc5fe
jisan06 <jisanahmed06@gmail.com>
2021-12-17 15:04:27 +0600

    order list in admin

---
+++ classes/Order.php
@@ -0,0 +1,32 @@
+<?php
+	$filepath = realpath(dirname(__FILE__));
+	include_once ($filepath.'/../lib/Database.php');
+	include_once ($filepath.'/../helpers/Format.php');
+
+?>
+
+<?php 
+	class Order{
+		
+		private $db;
+		private $fm;
+		
+	public function __construct()
+		{
+			$this->db = new Database();
+			$this->fm = new Format();
+		}
+
+		public function orderList(){
+			$query = "SELECT tbl_order.*, tbl_product.name as productName, tbl_customer.name as customerName from tbl_order
+			INNER JOIN tbl_product 
+			ON tbl_order.productId = tbl_product.id
+			INNER JOIN tbl_customer 
+			ON tbl_order.customerId = tbl_customer.id
+			order by id desc ";
+			$result = $this->db->select($query);
+			return $result;
+		}
+	}
+
+?>
